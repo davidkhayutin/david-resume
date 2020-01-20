509059e5c8b62b28592aa15dd9b616229b545520
davidkhayutin <david.khayutin@gmail.com>
2020-01-19 18:39:25 -0800

    updates

--- js/main.js
+++ js/main.js
@@ -263,6 +263,8 @@
   		}
 
 	});
+	
+	/* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
 
 
  	/*----------------------------------------------------- */
@@ -287,5 +289,193 @@
 		}		
 
 	});		
+	  $(window).scroll(function () { 
+    if ($(window).scrollTop() > 650) {
+      $('#nav_bar').slideDown(800).addClass('navbar-fixed');;
+    }
+    if ($(window).scrollTop() < 650) {
+      $('#nav_bar').removeClass('navbar-fixed').slideUp(800);;
+    }
+  });
 
 })(jQuery);
+
+// Get the modal
+var modal = document.getElementById("myModal");
+
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn");
+
+// When the user clicks on the button, open the modal
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+
+
+// Get the modal
+var modal2 = document.getElementById("myModal2");
+
+// Get the button that opens the modal
+var btn2 = document.getElementById("myBtn2");
+
+// When the user clicks on the button, open the modal
+btn2.onclick = function() {
+  modal2.style.display = "block";
+}
+
+// Get the modal
+var modal3 = document.getElementById("myModal3");
+
+// Get the button that opens the modal
+var btn3 = document.getElementById("myBtn3");
+
+// When the user clicks on the button, open the modal
+btn3.onclick = function() {
+  modal3.style.display = "block";
+}
+
+// Get the modal
+var modal4 = document.getElementById("myModal4");
+
+// Get the button that opens the modal
+var btn4 = document.getElementById("myBtn4");
+
+// When the user clicks on the button, open the modal
+btn4.onclick = function() {
+  modal4.style.display = "block";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";	
+  }
+  if (event.target == modal2) {
+    modal2.style.display = "none";
+  }
+   if (event.target == modal3) {
+    modal3.style.display = "none";
+  }
+   if (event.target == modal4) {
+    modal4.style.display = "none";
+  }
+}
+
+var slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+}
+
+var slideIndex2 = 1;
+showSlide2(slideIndex2);
+
+// Next/previous controls
+function plusSlide2(n) {
+  showSlide2(slideIndex2 += n);
+}
+
+// Thumbnail image controls
+function currentSlide2(n) {
+  showSlide2(slideIndex2 = n);
+}
+
+function showSlide2(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides2");
+  var dots = document.getElementsByClassName("dot2");
+  if (n > slides.length) {slideIndex2 = 1}
+  if (n < 1) {slideIndex2 = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex2 - 1].style.display = "block";
+  dots[slideIndex2 - 1].className += " active";
+}
+
+
+var slideIndex3 = 1;
+showSlide3(slideIndex3);
+
+// Next/previous controls
+function plusSlide3(n) {
+  showSlide3(slideIndex3 += n);
+}
+
+// Thumbnail image controls
+function currentSlide3(n) {
+  showSlide3(slideIndex3 = n);
+}
+
+function showSlide3(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides3");
+  var dots = document.getElementsByClassName("dot3");
+  if (n > slides.length) {slideIndex3 = 1}
+  if (n < 1) {slideIndex3 = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex3 - 1].style.display = "block";
+  dots[slideIndex3 - 1].className += " active";
+}
+
+
+var slideIndex4 = 1;
+showSlide4(slideIndex4);
+
+// Next/previous controls
+function plusSlide4(n) {
+  showSlide4(slideIndex4 += n);
+}
+
+// Thumbnail image controls
+function currentSlide4(n) {
+  showSlide4(slideIndex4 = n);
+}
+
+function showSlide4(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides4");
+  var dots = document.getElementsByClassName("dot4");
+  if (n > slides.length) {slideIndex4 = 1}
+  if (n < 1) {slideIndex4 = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex4 - 1].style.display = "block";
+  dots[slideIndex4 - 1].className += " active";
+}
